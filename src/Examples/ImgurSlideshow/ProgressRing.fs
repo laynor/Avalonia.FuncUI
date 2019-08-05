0f545af7675464f2941a2348fa09ea95ea2077e8
Alessandro Piras <laynor@gmail.com>
2019-08-05 13:21:13 +0200

    Code cleanup.

--- src/Examples/ImgurSlideshow/ProgressRing.fs
+++ src/Examples/ImgurSlideshow/ProgressRing.fs
@@ -27,15 +27,14 @@ type ProgressRing() as this =
             match this.Content :?> Canvas with
             | null -> ()
             | canvas ->
-                let tr = RotateTransform(Angle = _alpha)
+                canvas.RenderTransform <- RotateTransform(Angle = _alpha)
                 _alpha <- _alpha + 360.0 / 120.0
-                canvas.RenderTransform <- tr
                 let s = canvas.Bounds.Size
                 canvas.RenderTransformOrigin <- RelativePoint(s.Width / 2.0,
                                                               s.Height / 2.0,
                                                               RelativeUnit.Absolute)
-       _timer.Tick.Add(onTimer)
-       _timer.Start()
+       _timer.Tick.Add onTimer
+       _timer.Start ()
        this.LayoutUpdated.Add this.Populate
 
 
@@ -48,9 +47,9 @@ type ProgressRing() as this =
         let r = Rectangle(Width = size, Height = size / 2.0, Fill = this.Foreground)
         r.RenderTransform <- RotateTransform(Angle =  180.0 * (alpha / Math.PI))
 
-        let hs = canvas.Bounds.Size / 2.0
+        let hs   = canvas.Bounds.Size / 2.0
         let left = hs.Width + radius * cos alpha - r.Width / 2.0
-        let top = hs.Height + radius * sin alpha - r.Height / 2.0
+        let top  = hs.Height + radius * sin alpha - r.Height / 2.0
         Canvas.SetLeft(r, left)
         Canvas.SetTop(r, top)
         r
@@ -65,7 +64,6 @@ type ProgressRing() as this =
 
 
     member this.InitializeComponent () =
-        printfn "InitializeComponent called!!"
         AvaloniaXamlLoader.Load this
 
 [<AutoOpen>]
@@ -76,6 +74,5 @@ module ProgressRingDSLExtensions =
 
 
   type Attrs with
-
       static member inline radius<'T when 'T : (member set_Radius : float -> unit)>(value: float) : TypedAttr<'T> =
           TypedAttr<_>.Property { Name = "Radius"; Value = value }
